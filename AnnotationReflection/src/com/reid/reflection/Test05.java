574bf069e7ed1fb1e2f7e2ea6d69a3f2b7b84e36
ruibol <mvp330@qq.com>
2021-05-07 00:47:17 +0800

    Add new code

--- AnnotationReflection/src/com/reid/reflection/Test05.java
+++ AnnotationReflection/src/com/reid/reflection/Test05.java
@@ -12,6 +12,18 @@ public class Test05 {
     public static void main(String[] args) {
         A a = new A();
         System.out.println(A.m);
+
+        /*
+         1.加载到内存，会产生一个类对应Class对象
+         2.链接，链接结束后m=0
+         3.初始化
+            <clinit>(){
+                System.out.println("A类静态代码块初始化");
+                m = 300;
+                m = 100;
+            }
+            代码块合并所以m=100
+         **/
     }
 }
 
